8f6efb4e7b1262948cd893eaafe763c2f1e2a46e
yasminsc <yasmin.schryer@gmail.com>
2020-10-06 17:22:36 +0300

    clean up

--- dot_on_line.py
+++ dot_on_line.py
@@ -44,27 +44,3 @@ def is_dot_on_line(dota, dotb, dotc):
             return 1
         else:
             return 0
-
-
-# # tests
-# def print_result(a, b, c):
-#     if is_dot_on_line(a, b, c) == 1:
-#         print("dot c (" + str(c.x1) + "," + str(c.x2) + ") is on the line a-b: a(" + str(a.x1) + "," + str(a.x2) + ") , b(" + str(b.x1) + "," + str(b.x2) + ")")
-#     else:
-#         print("dot c (" + str(c.x1) + "," + str(c.x2) + ") is not on the line a-b a(" + str(a.x1) + "," + str(a.x2) + ") , b(" + str(b.x1) + "," + str(b.x2) + ")")
-#
-#
-# a = Dot(1, 9)
-# b = Dot(-1, 1)
-# c = Dot(0, 3)
-# d = Dot(0, 3)
-# e = Dot(1, 2)
-# f = Dot(3, -7)
-# g = Dot(1, 4)
-# h = Dot(1, 3)
-#
-# print_result(a, b, c)
-# print_result(a, d, c)
-# print_result(d, b, c)
-# print_result(g, e, h)
-# print_result(g, e, d)
